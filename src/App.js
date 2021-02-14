83046e87b8d68201a0da4682a2d243b997a04b94
oliver <oliver.wu.1987@gmail.com>
2021-02-14 10:52:23 +0000

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn 1React1
+          Learn 1React
         </a>
       </header>
     </div>
